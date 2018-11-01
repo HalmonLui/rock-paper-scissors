2ee8ca73c7fd779e0c3f0e8cc2f930305e1accbb
halmonlui <halmonlui@gmail.com>
2018-11-01 01:08:31 -0400

    Added win conditions and game reset

--- components/RPS/RPS.js
+++ components/RPS/RPS.js
@@ -14,6 +14,8 @@ export default class RPS extends Component {
       countdown: '',
       playerReady: 0,
       opponentReady: 0,
+      playerSelect: '',
+      opponentSelect: '',
     };
   }
 
@@ -21,13 +23,7 @@ export default class RPS extends Component {
     this.setState({countdown: 'Get Ready!'})
   }
 
-  jab = () => {
-    let deathKnight = this.state.deathKnight;
-    let userSkills = this.state.userData.nom.skills.skill3;
-    deathKnight.currentHP = deathKnight.currentHP - userSkills.damage;
-    this.deadCheck(deathKnight);
-  }
-
+  // onPress for players being ready which starts the countdown
   ready = ( who ) => {
     if(who == "opponent"){
       this.setState({opponentReady: 1}, () => {
@@ -47,6 +43,7 @@ export default class RPS extends Component {
     }
   }
 
+  // Countdown to begin match
   begin = () => {
     var counter = 2;
     var i = setInterval(() => {
@@ -61,33 +58,97 @@ export default class RPS extends Component {
     }, 1000);
   }
 
+  // User select rock paper or scissors
   select = (who, what) => {
-
+    var select = who + "Select";
+    const { navigate } = this.props.navigation;
+    let playerSelect = this.state.playerSelect;
+    let opponentSelect = this.state.opponentSelect;
+    this.setState({ [select]: what}, () => {
+      if(what == playerSelect && playerSelect == opponentSelect){
+        this.setState({countdown: "TIE"}, () => {
+          var i = setInterval(() => {
+            this.setState({
+              playerSelect: "",
+              opponentSelect: "",
+              playerReady: 0,
+              opponentReady: 0,
+            }, () => {navigate('Main')});
+          }, 3000);
+        });
+      }
+      else if ((playerSelect == "rock" && opponentSelect == "paper") || (playerSelect == "scissors" && opponentSelect == "rock") || (playerSelect == "paper" && opponentSelect == "scissors")){
+        this.setState({countdown: "LOSE!"});
+        var i = setInterval(() => {
+          this.setState({
+            playerSelect: "",
+            opponentSelect: "",
+            playerReady: 0,
+            opponentReady: 0,
+          }, () => {navigate('Main')});
+        }, 3000);
+      }
+      else if ((opponentSelect == "rock" && playerSelect == "paper") || (opponentSelect == "scissors" && playerSelect == "rock") || (opponentSelect == "paper" && playerSelect == "scissors")){
+        this.setState({countdown: "WIN!"});
+        var i = setInterval(() => {
+          this.setState({
+            playerSelect: "",
+            opponentSelect: "",
+            playerReady: 0,
+            opponentReady: 0,
+          }, () => {navigate('Main')});
+        }, 3000);
+      }
+    });
   }
 
   render(){
     return (
       <ImageBackground keyboardShouldPersistTaps='always' source={require('../../images/saoBackground.png')} style={styles.container}>
         <View style={styles.enemySide}>
-          {(this.state.countdown == "Get Ready!") && <TouchableOpacity style={styles.readyButton} onPress={() => this.ready('opponent')}>
+          {(this.state.opponentReady == 0) && <TouchableOpacity style={styles.readyButton} onPress={() => this.ready('opponent')}>
             <Text style={styles.readyText}> Ready </Text>
           </TouchableOpacity>}
           <View style={styles.enemyRPSSlotContainer}>
-          <RPSSlot name='Scissors' source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 1 || this.state.countdown == "GO!")) ? scissors : null} onPress = {() => this.select('opponent', 'rock')}/>
-          <RPSSlot name='Paper' source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 2 || this.state.countdown == "GO!")) ? paper : null}/>
-          <RPSSlot name='Rock' source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 3 || this.state.countdown == "GO!")) ? rock : null}/>
+            <RPSSlot name='Scissors'
+              source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 1 || this.state.countdown == "GO!")) ? scissors : null}
+              onPress = {() => this.select('opponent', 'scissors')}
+            />
+            <RPSSlot name='Paper'
+              source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 2 || this.state.countdown == "GO!")) ? paper : null}
+              onPress = {() => this.select('opponent', 'paper')}
+            />
+            <RPSSlot name='Rock'
+              source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 3 || this.state.countdown == "GO!")) ? rock : null}
+              onPress = {() => this.select('opponent', 'rock')}
+            />
           </View>
           {/*<Image source = {isDead ? Firework : DeathKnight} style = {styles.enemySprite} />*/}
         </View>
-        <Text style={styles.countdown}> {this.state.countdown} </Text>
+
+        <View style={styles.centerArea}>
+          <Text style={styles.select}> {this.state.opponentSelect} </Text>
+          <Text style={styles.countdown}> {this.state.countdown} </Text>
+          <Text style={styles.select}> {this.state.playerSelect} </Text>
+        </View>
+
         <View keyboardShouldPersistTaps='always' style={styles.userSide}>
           {/*<Image source = {Kirito} style = {styles.userSprite} />*/}
           <View keyboardShouldPersistTaps='always' style={styles.RPSSlotContainer}>
-            <RPSSlot name='Rock' source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 3 || this.state.countdown == "GO!")) ? rock : null}/>
-            <RPSSlot name='Paper' source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 2 || this.state.countdown == "GO!")) ? paper : null}/>
-            <RPSSlot name='Scissors' source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 1 || this.state.countdown == "GO!")) ? scissors : null}/>
+            <RPSSlot name='Rock'
+              source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 3 || this.state.countdown == "GO!")) ? rock : null}
+              onPress = {() => this.select('player', 'rock')}
+            />
+            <RPSSlot name='Paper'
+              source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 2 || this.state.countdown == "GO!")) ? paper : null}
+              onPress = {() => this.select('player', 'paper')}
+            />
+            <RPSSlot name='Scissors
+              ' source={(this.state.playerReady && this.state.opponentReady && (this.state.countdown == 1 || this.state.countdown == "GO!")) ? scissors : null}
+              onPress = {() => this.select('player', 'scissors')}
+            />
           </View>
-          {(this.state.countdown == "Get Ready!") && <TouchableOpacity style={styles.readyButton} onPress={() => this.ready('player')}>
+          {(this.state.playerReady == 0) && <TouchableOpacity style={styles.readyButton} onPress={() => this.ready('player')}>
             <Text style={styles.readyText}> Ready </Text>
           </TouchableOpacity>}
         </View>
@@ -122,6 +183,7 @@ const styles = StyleSheet.create({
     textShadowRadius: 10,
   },
   countdown: {
+    flex: 1,
     fontSize: 30,
     fontWeight: 'bold',
     alignSelf: 'center',
@@ -129,6 +191,15 @@ const styles = StyleSheet.create({
     textShadowColor: 'rgba(0, 0, 0, 0.75)',
     textShadowOffset: {width: -1, height: 1},
     textShadowRadius: 10,
+    textAlign: 'center',
+  },
+  select: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  centerArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
   enemySide: {
     flex: 1,
